258dc4aab303c417c30467cf98ca17e4ee238683
Melissa <wolfaholic@users.noreply.github.com>
2021-04-04 18:33:28 -0700

    getting codegrade to work

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters test</h1>
+      <h1 className="Header">Characters test codegrade</h1>
     </div>
   );
 }
